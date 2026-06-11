a2b12e6b5ab54bd06c9e784fdc71fdf85dac8e63
kish0ck <3kistone@gmail.com>
2026-06-11 17:39:54 +0900

    Setup GitHub Pages deployment
    - Enable static export in Next.js
    - Configure basePath for GitHub Pages (/VibeCoding-hns-2)
    - Add GitHub Actions workflow for automated deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: '/VibeCoding-hns-2',
+  assetPrefix: '/VibeCoding-hns-2/',
 }
 
 module.exports = nextConfig
